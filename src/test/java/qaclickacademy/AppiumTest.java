a0f3c3947d1dc5803ef5d02370858c6fed68fc59
rajani <rajnisharan@gmail.com>
2020-05-20 23:57:59 -0400

    entire directory is  added to repository

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeApAndroid()
+	{
+		System.out.println("nativeapp");
+	}
+
+@Test
+public void IOSApps()
+{
+System.out.println("IOSApps");
+	
+}
+
+
+	
+	
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+//how to execute all test cases from test folder using -maven with mvn test 
+//testng xml file from maven
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}
+	
+
+	
+}

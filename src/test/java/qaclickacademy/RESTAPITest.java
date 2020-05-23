7d3a6ae46c6276d193758b5659424ae5bbe426f7
rajani <rajnisharan@gmail.com>
2020-05-23 01:26:22 -0400

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -9,7 +9,10 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
-		
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
 		
 	}
 	
@@ -17,6 +20,8 @@ public class RESTAPITest {
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+		
+		
 	}
 	
 
